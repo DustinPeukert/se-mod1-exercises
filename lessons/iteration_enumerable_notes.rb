146a151234c9769c8449123960538708acb46e33
Dustin Peukert <dpeukert04@gmail.com>
2024-10-10 15:04:09 -0400

    Lesson 3 Enumerable/Iteration notes

---
+++ lessons/iteration_enumerable_notes.rb
@@ -0,0 +1,38 @@
+# Iteration: simple way to access elements in a collection
+# allows access to all, not just one element
+# used when we need to perform an operation on every element
+#
+
+# bounce_letters = ["K", "a", "n", "g", "a", "r", "o", "o\n"]
+
+# bounce_letters.each do |bounce_letter|
+#   print bounce_letter
+# end
+
+# names = ['megan', 'mike', 'ian']
+# capitalized_names = []
+
+# names.each do |name|
+#   capitalized_names << name.capitalize
+# end
+
+# puts capitalized_names
+
+# numbers = [1, 8, 19, 21, 29, 31, 34]
+
+# found = numbers.find do |number|
+#   number > 20
+# end
+
+# puts numbers
+# puts "Result:"
+# puts found
+
+# names = ["alice", "bob", "charlie"]
+
+# capitalized_names = names.map do |name|
+#   name.capitalize
+# end
+
+# puts names
+# puts capitalized_names

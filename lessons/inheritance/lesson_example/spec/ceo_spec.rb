3df101558f46282f8f05cd6987531615882ca2c1
Dustin Peukert <dpeukert04@gmail.com>
2024-11-01 11:03:12 -0400

    Lesson 13 Inheritance

--- lessons/inheritance/lesson_example/spec/ceo_spec.rb
+++ lessons/inheritance/lesson_example/spec/ceo_spec.rb
@@ -15,7 +15,16 @@ RSpec.describe Ceo do
 
   describe "Has a name, ID, and total_compensation" do
     # Write tests to prove that a Ceo instance can have a Name and ID number, like any other Employee.
+    it "has name and id" do
+      ceo = Ceo.new("John", 23154)
+      expect(employee.name).to eq("John")
+      expect(employee.id).to eq(23154)
+    end
     # Also, the CEO should be able to receive a total compensation. Write at least one test that proves they can access this method.
+    it "can calculate a total compensation" do
+      ceo = Ceo.new("John", 23154)
+      expect(employee).to respond_to(:total_compensation)
+    end
   end
 
 end
